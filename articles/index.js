029310591d8d311504ff9aa80f701d2288d7a8a7
aelkhamsi <achrafelkhamsi@gmail.com>
2021-03-21 13:48:21 +0100

    Add cors & body-parser middlewares for the articles microservice

--- articles/index.js
+++ articles/index.js
@@ -1,10 +1,28 @@
-//import dependencies
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 const db = require('./config');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+/////////////////////
+//// MIDDLEWARES ////
+/////////////////////
+
+app.use(bodyParser.json()) //Parsed data is populated on the request object (i.e. req.body).
+app.use(bodyParser.urlencoded( { extended: true }))
+app.use( (req, res, next ) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Requested-With, Accept, x-access-token')
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+  }
+  res.header('Content-Type', 'application/json') //REST-API: we only send back json data
+  next()
+})
+
+////////////////
+//// ROUTES ////
+////////////////
 
 app.get('/', (req, res) => {
     res.send("<h1> Hello World! </h1>");
--- auth/index.js
+++ auth/index.js
@@ -4,9 +4,34 @@ const db = require('./config');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-/////////////////////////////////
-//// Database Initialization ////
-///////////////////////////////// 
+
+/////////////////////
+//// MIDDLEWARES ////
+/////////////////////
+
+app.use(bodyParser.json()) //Parsed data is populated on the request object (i.e. req.body).
+app.use(bodyParser.urlencoded( { extended: true }))
+app.use( (req, res, next ) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Requested-With, Accept, x-access-token')
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+  }
+  res.header('Content-Type', 'application/json') //REST-API: we only send back json data
+  next()
+})
+
+
+////////////////
+//// ROUTES ////
+////////////////
+
+require('./routes/auth.routes.js')(app);
+
+
+///////////////////////////
+/// DATABASE CONNECTION ///
+///////////////////////////
 
 db.connect((err) => {
   if (err) {
@@ -34,37 +59,10 @@ setInterval(function () { //to keep the connection alive, make frequent quries t
 //   console.log("Users Table Created...");
 // })
 
-/////////////////////
-//// Middlewares ////
-/////////////////////
-
-app.use(bodyParser.json()) //Parsed data is populated on the request object (i.e. req.body).
-app.use(bodyParser.urlencoded( { extended: true }))
-app.use( (req, res, next ) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Requested-With, Accept, x-access-token')
-  if (req.method == 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-  }
-  res.header('Content-Type', 'application/json') //REST-API: we only send back json data
-  next()
-})
-// app.use((req, res, next) => {
-//   req.db = db
-//   next();
-// })
-
 
 ////////////////
-//// Routes ////
+//// LISTEN ////
 ////////////////
-
-require('./routes/auth.routes.js')(app);
-
-
-///////////////////
-//// Listening ////
-///////////////////
 
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
