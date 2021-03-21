42515ddc3c536c6d36a3cbfc3cf0a848b80ef742
aelkhamsi <achrafelkhamsi@gmail.com>
2021-03-21 01:59:40 +0100

    Create login controller

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -1,9 +1,60 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
 
 exports.login = (req, res) => {
-  res.send("<h1> Login </h1>")
+  const db = req.db;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email && password) {
+    let sql = `SELECT * FROM users WHERE email = ?`;
+    let query = db.query(sql, email, (err, result) => {
+      if (err) {
+          console.log(err);
+          res
+            .status(500)
+            .json({errorMessage: "Internal server error. Please try another time1"})
+      }
+
+      if (result.length != 0) {
+        const hash = result[0].password;
+        bcrypt.compare(password, hash, function(err, success) {
+          if (success) 
+          { //Good password
+            const user = {
+              username: result[0].username,
+              email: result[0].email
+            }
+            jwt.sign(user, "iamthegreatestaliveandiamhumble", function(err, token) { //TODO: create env variable for the secret
+              res
+                .status(200)
+                .json({id: result[0].id, username: user.username, access_token: token})
+            })
+          }
+          else 
+          { //Wrong Password
+            res
+            .status(404)
+            .json({errorMessage: "Incorrect Email or Password"})
+          }
+        });
+      } 
+      else 
+      { //Wrong Email (email doesn't exist)
+        res
+          .status(404)
+          .json({errorMessage: "Incorrect Email or Password"})
+      }
+    });
+  }
+  else
+  { // Missing fields
+    res
+      .status(400)
+      .json({errorMessage: "Missing fields"})
+  }
 };
 
 
@@ -15,7 +66,8 @@ exports.signup = (req, res) => {
   const role = req.body.role;
 
 
-  if (username && email && password && role) {
+  if (username && email && password && role) 
+  {
     let sql = `SELECT * FROM users WHERE email = ?`;
     let query = db.query(sql, email, (err, result) => {
       if (err) {
@@ -25,13 +77,13 @@ exports.signup = (req, res) => {
             .json({errorMessage: "Internal server error. Please try another time1"})
       }
       if (result.length > 0) 
-      { //email already exists
+      { // Email already exists
         res
           .status(400)
           .json({errorMessage: "This email is already used"})
       } 
       else 
-      { 
+      { // Existing Email
         bcrypt.hash(password, saltRounds, function(err, hash) {
           let user = {
             username: username, 
@@ -48,6 +100,7 @@ exports.signup = (req, res) => {
                 .status(500)
                 .json({errorMessage: "Internal server error. Please try another time2"})
             }
+            // Success
             res
               .status(200)
               .json({message: "User added"})
@@ -56,7 +109,9 @@ exports.signup = (req, res) => {
         });
       }
     });
-  } else {
+  } 
+  else 
+  { // Missing Fields
       res
         .status(400)
         .json({errorMessage: "Missing fields"})
