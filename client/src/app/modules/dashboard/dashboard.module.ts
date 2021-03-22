682b02346337aafc151511ef956b620031db619a
aelkhamsi <achrafelkhamsi@gmail.com>
2021-03-22 15:20:01 +0100

    create a split screen for the dashboard page

--- client/src/app/modules/dashboard/dashboard.module.ts
+++ client/src/app/modules/dashboard/dashboard.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BlogCardComponent } from './components/blog-card/blog-card.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    HeaderComponent
+    HeaderComponent,
+    BlogCardComponent
   ],
   imports: [
     CommonModule,
